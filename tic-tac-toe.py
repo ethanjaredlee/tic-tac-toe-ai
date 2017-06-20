8af33e9e615661fe9940e235c22c988e9b4e13ed
ethlee4 <ethanlee2020@u.northwestern.edu>
2017-06-19 22:13:52 -0500

    "started on the recursive ai function"

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,35 +1,73 @@
-class Space():
-    # Spaces on a board
-    def __init__(self, i):
-        self.visited = ""
+'''
 
+Quick notation:
+    * 0 + n*2 stands for ai turn
+    * 1 + n*2 stands for opponent turn
 
-    def select(self, player):
-        self.visited = player
+'''
 
-#FIX THIS
+#IMPROVE THIS
 def winner(pieces):
-    if ((pieces[0] != "" && pieces[0] == pieces[3] && pieces[3] == pieces[6]) || # check horizontal
-        (pieces[1] != "" && pieces[1] == pieces[4] && pieces[4] == pieces[7]) ||
-        (pieces[2] != "" && pieces[2] == pieces[5] && pieces[5] == pieces[8]) ||
-        (pieces[0] != "" && pieces[0] == pieces[1] && pieces[1] == pieces[2]) || # check vertical
-        (pieces[3] != "" && pieces[3] == pieces[4] && pieces[4] == pieces[5]) ||
-        (pieces[6] != "" && pieces[6] == pieces[7] && pieces[7] == pieces[8]) ||
-        (pieces[2] != "" && pieces[2] == pieces[4] && pieces[4] == pieces[6]) || # check diagonal
-        (pieces[0] != "" && pieces[0] == pieces[4] && pieces[4] == pieces[8]))
+    if (pieces[0] != "" && pieces[0] == pieces[3] && pieces[3] == pieces[6]):
+        return pieces[0] # check horizontal
+    if (pieces[1] != "" && pieces[1] == pieces[4] && pieces[4] == pieces[7]):
+        return pieces[1]
+    if (pieces[2] != "" && pieces[2] == pieces[5] && pieces[5] == pieces[8]):
+        return pieces[2]
+    if (pieces[0] != "" && pieces[0] == pieces[1] && pieces[1] == pieces[2]):
+        return pieces[0]
+    if (pieces[3] != "" && pieces[3] == pieces[4] && pieces[4] == pieces[5]):
+        return pieces[3]
+    if (pieces[6] != "" && pieces[6] == pieces[7] && pieces[7] == pieces[8]):
+        return pieces[6]
+    if (pieces[2] != "" && pieces[2] == pieces[4] && pieces[4] == pieces[6]):
+        return pieces[2]
+    if (pieces[0] != "" && pieces[0] == pieces[4] && pieces[4] == pieces[8]):
+        return pieces[0]
+    return "no winner"
+
+def scoring(array):
+    if winner(array) == 'ai':
+        return 10
+    if winner(array) == 'opponent':
+        return -10
+    else:
+        return 0
 
 
+def best_choice(array):
+    finalScores = best_choice_helper(array, [0] * 9, 0)
+    return finalScores.index(max(finalScores))
+
+def best_choice_helper(array, scores, turn):
+    # recurses and finds the scores of all possible options
+    copy = array[:]
+    empty = False
+
+    # check whose turn it is
+    if (turn % 2 == 0): # ai turn
+
+        for i in range(0,9):
+            if copy[i] == "":
+                copy[i] = "o"
+                scores[i] = scores[i] + scoring(copy)
+                best_choice(copy, scores, turn + 1)
+                copy[i] = ""
+                empty = True
+
+    if (turn % 2 == 1): # opponent turn
+        for i in range(0,9):
+            if copy[i] == "":
+                copy[i] = "x"
+                scores[i] = scores[i] + scoring(copy)
+                best_choice(copy, scores, turn + 1)
+                copy[i] = ""
+                empty = True
+
+    if not empty:
+        return scores
+
+best_choice(["x", "", "", ])
 
 def draw_board():
-    pieces = [Space() for i in range(0,9)]
-
-draw_board()
-
-def ai_helper(spot, score):
-    if winner(spot) == 'ai':
-        return score + 1
-    if winner(spot) == 'opponent':
-        return score - 1
-    for piece in spot:
-        if piece.visited = "":
-            ai_helper()
+    pieces = ["" for i in range(0,9)]
